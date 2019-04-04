a1e3ab5be907e6079b52114e7b23de9e6a155787
Adnan Sabbir <adnansabbir2012@gmail.com>
2019-04-05 02:41:53 +0600

    Recursive Reverse String

--- reverse_string_recursive.py
+++ reverse_string_recursive.py
@@ -1,6 +1,22 @@
-def reverse(s):
-	if len(s) == 1:
-		return s
-	return s[-1]+reverse(s[:len(s)-1])
+from nose.tools import assert_equal
 
-print(reverse('Hello Hi'))
+
+def recursive_reverse(s):
+    if len(s) < 1:
+        return s
+    return s[-1] + recursive_reverse(s[:len(s) - 1])
+
+
+def test_recursive_reverse():
+    assert_equal(recursive_reverse(''), ''[::-1])
+    assert_equal(recursive_reverse('Hello World'), 'Hello World'[::-1])
+    assert_equal(recursive_reverse('0123456789'), '0123456789'[::-1])
+    assert_equal(recursive_reverse('0'), '0'[::-1])
+    assert_equal(recursive_reverse('a'), 'a'[::-1])
+    assert_equal(recursive_reverse('a.-'), 'a.-'[::-1])
+    assert_equal(recursive_reverse(' . . . - \    '), ' . . . - \    '[::-1])
+    assert_equal(recursive_reverse('    '), '    '[::-1])
+    print("All test passed")
+
+
+test_recursive_reverse()
